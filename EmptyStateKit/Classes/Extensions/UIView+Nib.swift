35bb1b02a2d4e46a93eadf9d16adef818aaf57ad
Julien Di Marco <juliendimarco@gmail.com>
2021-08-25 03:36:18 +0200

    feat(layout): Update layout to use layoutAnchor for simpler code

--- EmptyStateKit/Classes/Extensions/UIView+Nib.swift
+++ EmptyStateKit/Classes/Extensions/UIView+Nib.swift
@@ -25,10 +25,10 @@ extension UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.frame = container.frame
         container.addSubview(self)
-        
-        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+
+      self.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+      self.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
+      self.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+      self.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
     }
 }
